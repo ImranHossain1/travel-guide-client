8aae7273aaf8e318ac0303a3d957aadb6da198b5
ImranHossain1 <imranhossain1402@gmail.com>
2022-06-23 03:51:01 +0200

    Final Deploy

--- src/Pages/Dashboard/DeleteConfirmModal.js
+++ src/Pages/Dashboard/DeleteConfirmModal.js
@@ -27,7 +27,7 @@ const DeleteConfirmModal = ({deletingDestination, refetch, setDeletingDestinatio
             <div className="modal modal-bottom sm:modal-middle">
             <div className="modal-box">
                 <h3 className="font-bold text-lg text-red-500">Are you sure you want to delete {destinationName}</h3>
-                <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                <p className="py-4">Make sure if you delete this destination would create some issue while people want to book.</p>
                 <div className="modal-action">
                     <button onClick={()=>handleDelete()} className="btn btn-xs btn-error">
                         Delete
