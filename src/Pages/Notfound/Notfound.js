cc71e97aca1f4184249807ebf9416995535b138c
ImranHossain1 <imranhossain1402@gmail.com>
2022-07-06 19:16:19 +0200

    warning updated

--- src/Pages/Notfound/Notfound.js
+++ src/Pages/Notfound/Notfound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     return (
         <div className='flex flex-col justify-center items-center mb-12'>
             <img src={notfound} alt=""  className='w-full min-h-screen mb-5'/>
-            <PrimaryButton  as={Link} to="/" className='rounded-pill button px-5 py-3'>Go Back</PrimaryButton>
+            <Link to='/'><PrimaryButton className='rounded-pill button px-5 py-3'>Go Back</PrimaryButton></Link>
         </div>
     );
 };
