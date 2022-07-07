cb7cee25a32a705bc3fe9f67813e5328d71309f5
ImranHossain1 <imranhossain1402@gmail.com>
2022-07-08 00:10:27 +0200

    Dashboard problem resolved

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -66,16 +66,18 @@ const Dashboard = () => {
     //console.log(messages)
     return (
         <div className="drawer drawer-mobile  ">
-            <input id="my-drawer-10" type="checkbox" className="drawer-toggle" />
+            <input id="myDrawerDashboard" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content  mt-5 ml-2 ">
                 {/* <!-- Page content here --> */}
-                <label htmlFor="my-drawer-10" className="btn btn-primary drawer-button lg:hidden">Menu</label>
+                <label htmlFor="myDrawerDashboard" className="btn btn-primary drawer-button lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
                 <div className='flex justify-center  '>
                     <Outlet></Outlet>
                 </div>
             </div> 
-            <div className="drawer-side-50">
-                <label htmlFor="my-drawer-10" className="drawer-overlay"></label> 
+            <div className="drawer-side">
+                <label htmlFor="myDrawerDashboard" className="drawer-overlay"></label> 
                 <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content ">
                     
                 {/* <!-- Sidebar content here --> */}
